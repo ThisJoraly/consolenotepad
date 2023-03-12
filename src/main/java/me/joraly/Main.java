412cc3f279828a11c14a47ab1e9b01a3ff3e544b
Artyom Joraly <tyomajoraly@gmail.com>
2023-03-12 14:46:21 +0300

    Initial commit

---
+++ src/main/java/me/joraly/Main.java
@@ -0,0 +1,25 @@
+package me.joraly;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = 1;
+
+        while(true) {
+            int x = i;
+            Integer.toString(x);
+            System.out.printf("%3s|", String.valueOf(x));
+            String command = scanner.nextLine();
+            i++;
+            switch (command){
+                case "!q" -> { return;
+                }
+                default -> {
+                    continue;
+                }
+            }
+        }
+    }
+}
