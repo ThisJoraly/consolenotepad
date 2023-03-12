f25f6092950e2fc31216cb1b384db592c28ca5f0
Artyom Joraly <tyomajoraly@gmail.com>
2023-03-12 16:06:22 +0300

    ver. 2

--- src/main/java/me/joraly/Main.java
+++ src/main/java/me/joraly/Main.java
@@ -1,9 +1,15 @@
 package me.joraly;
 //v1
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
+import java.io.PrintWriter;
 
 public class Main {
+    static String s = File.separator;
+    static String path = "src"+s+"main"+s+"resources"+s + "out"+ s;
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
         Scanner scanner = new Scanner(System.in);
         int i = 1;
 
@@ -14,10 +20,30 @@ public class Main {
             String command = scanner.nextLine();
             i++;
             switch (command){
-                case "!q" -> { return;
+                case "!q" -> {
+                    scanner.close();
+                    return;
+                }
+                case "!sq" -> {
+                    try {
+
+
+                        System.out.println("CN |Впишите название (с расширением файла)");
+                        String fileName = scanner.nextLine();
+                        File file = new File(path + fileName);
+                        if(file.exists())
+                            file.createNewFile();
+                        PrintWriter pw = new PrintWriter(file);
+                        pw.println(sb);
+                        pw.close();
+                        return;
+
+                    } catch(IOException e) {
+                        System.out.println("Error |" + e);
+                    }
                 }
                 default -> {
-                    continue;
+                    sb.append(command + "\n");
                 }
             }
         }
